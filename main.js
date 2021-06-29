53bf51112f5c7757c95d915eea27b8986bb1a00f
himani412 <77782996+himani412@users.noreply.github.com>
2021-06-29 06:19:02 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
-function preload(){}
+nosex=0;
+nosey=0;
+function preload(){
+    imagenose=loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
+}
 function setup(){
-    canvas=createCanvas(640,480);
-    canvas.position(110,250);
+    canvas=createCanvas(300,300);
+    canvas.center();
     video=createCapture(VIDEO);
+    video.size(300,300);
     video.hide();
-    tintcolour="";
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
 }
+function modelLoaded(){
+    console.log('PoseNet is Initialised');
+} 
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        nosex=results[0].pose.nose.x-10;
+        nosey=results[0].pose.nose.y-10;
+        console.log("nose x = "+nosex);
+        console.log("nose y = "+nosey);
+    }
+}  
+
 function draw(){
-    image(video,0,0,640,480);
-    tint(tintcolour);
+    image(video,0,0,300,300);
+image(imagenose,nosex,nosey,30,30);
 }
-function take_snapshot(){
+function takesnapshot(){
     save('Himani.png');
 }
-function filtertint(){
-    tintcolour=document.getElementById("colourname").value;
-}
-

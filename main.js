b1b69c297f76b83b2099760c74c51d860609a62f
himani412 <77782996+himani412@users.noreply.github.com>
2021-08-11 05:23:04 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,35 +1,19 @@
-nosex=0;
-nosey=0;
+img= "";
+
 function preload(){
-    imagenose=loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
+    img = loadImage('dog_cat.jpg');
 }
 function setup(){
-    canvas=createCanvas(300,300);
+    canvas= createCanvas(640,420);
     canvas.center();
-    video=createCapture(VIDEO);
-    video.size(300,300);
-    video.hide();
 
-    poseNet=ml5.poseNet(video,modelLoaded);
-    poseNet.on('pose',gotPoses);
 }
-function modelLoaded(){
-    console.log('PoseNet is Initialised');
-} 
-function gotPoses(results){
-    if(results.length>0){
-        console.log(results);
-        nosex=results[0].pose.nose.x-10;
-        nosey=results[0].pose.nose.y-10;
-        console.log("nose x = "+nosex);
-        console.log("nose y = "+nosey);
-    }
-}  
-
 function draw(){
-    image(video,0,0,300,300);
-image(imagenose,nosex,nosey,30,30);
-}
-function takesnapshot(){
-    save('Himani.png');
+    image(img,0,0,640,420);
+    fill("#ff6700");
+     text("Dog ", 45 ,75);
+    noFill();
+    stroke("#17684f");
+    rect(30,60,450,350);
+     
 }
